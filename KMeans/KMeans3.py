0c44c9e9094231f9408d7eb85ee0fb093689829e
Ravinda <ravindra.iitism@gmail.com>
2024-02-17 18:38:08 +0530

    KMeans model

---
+++ KMeans/KMeans3.py
@@ -0,0 +1,69 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("income.csv")
+df.head()
+
+plt.scatter(df.Age, df['Income($)'])
+plt.xlabel('Age')
+plt.ylabel('Income($)')
+
+km = KMeans(n_clusters=3)
+y_predicted = km.fit_predict(df[['Age', 'Income($)']])
+
+df['cluster'] = y_predicted
+df.head()
+
+df1 = df[df.cluster == 0]
+df2 = df[df.cluster == 1]
+df3 = df[df.cluster == 2]
+plt.scatter(df1.Age,df1['Income($)'],color='green')
+plt.scatter(df2.Age,df2['Income($)'],color='red')
+plt.scatter(df3.Age,df3['Income($)'],color='black')
+plt.scatter(km.cluster_centers_[:,0],km.cluster_centers_[:,1],color='purple',marker='*',label='centroid')
+plt.xlabel('Age')
+plt.ylabel('Income ($)')
+plt.legend()
+# plt.show()
+
+# scaling value in a range
+scaler = MinMaxScaler()
+scaler.fit(df[['Income($)']])
+df['Income($)'] = scaler.transform(df[['Income($)']])
+
+scaler.fit(df[['Age']])
+df['Age'] = scaler.transform(df[['Age']])
+
+plt.scatter(df.Age, df['Income($)'])
+# plt.show()
+
+km = KMeans(n_clusters=3)
+y_predicted = km.fit_predict(df[['Age','Income($)']])
+
+df['cluster'] = y_predicted
+df.head()
+print("Centroids: ", km.cluster_centers_)
+
+df1 = df[df.cluster == 0]
+df2 = df[df.cluster == 1]
+df3 = df[df.cluster == 2]
+plt.scatter(df1.Age,df1['Income($)'], color='green')
+plt.scatter(df2.Age,df2['Income($)'], color='red')
+plt.scatter(df3.Age,df3['Income($)'], color='black')
+plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], color='purple', marker='*', label='centroid')
+plt.legend()
+# plt.show()
+
+sse = []
+k_rng = range(1,10)
+for k in k_rng:
+    km = KMeans(n_clusters=k)
+    km.fit(df[['Age', 'Income($)']])
+    sse.append(km.inertia_)
+
+plt.xlabel('K')
+plt.ylabel('Sum of squared error')
+plt.plot(k_rng, sse)
+plt.show()
