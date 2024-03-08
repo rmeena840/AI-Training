8965c9e9603a5755487fe0f995fc61d270092f38
Ravinda <ravindra.iitism@gmail.com>
2024-03-08 15:41:09 +0530

    AI chat bot

--- NeuralNetwork/NeuralNetwork1.py
+++ NeuralNetwork/NeuralNetwork1.py
@@ -12,14 +12,14 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images/255.0
-test_images = test_images/255.0
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 model = keras.Sequential([
-	keras.layers.Flatten(input_shape=(28,28)),
-	keras.layers.Dense(128, activation="relu"),
-	keras.layers.Dense(10, activation="softmax")
-	])
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
@@ -31,7 +31,7 @@ print('\nTest accuracy:', test_acc)
 
 predictions = model.predict(test_images)
 
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(5, 5))
 for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i], cmap=plt.cm.binary)
