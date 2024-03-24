b4efb600c3a3e9c8a73966d8af66f2c291e7175f
Ravinda <ravindra.iitism@gmail.com>
2024-03-24 11:38:14 +0530

    Notes updated

--- GradientDescent.py
+++ GradientDescent.py
@@ -5,3 +5,9 @@
 # 4. Chain Rule
 # 5. Tensorboard for visualization and debugging
 # 6. Neuroevolutionary Algorithm or NEAT
+
+
+# Important notes:
+# 1. Feature scaling should be done after train and test split.
+# 2. Never use fit on test data. Use fit_transform on train data but never use this with test data. You can use transform.
+# 3. Whener there D number of dummy variables, then there should be D-1 number of dummy variables.
